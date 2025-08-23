f181f1fc3f5c78100c3a0eefddfa152b4b4f43e5
8x34tnxge <8x34tnxge@mozmail.com>
2025-08-24 00:54:18 +0800

    feat(message-generation): Prepare for Gemini API integration in ordering messages
    Adds an optional `gemini_api` parameter to `GeneratePrevOrderingMsg` and
    `GenerateOrderingNotificationMsg`. This introduces logic to
    conditionallyuse a message from the Gemini API, falling back to the
    existing randommessage generation if the API key is not provided.

--- message.go
+++ message.go
@@ -33,46 +33,78 @@ func GenerateKaomoji() string {
 	return kaomojis[randomIndex]
 }
 
-func GeneratePrevOrderingMsg() string {
-	msgs := []string{
-		"报餐时间到！",
-		"爱护身体是一种投资，要投资你的胃，让身体更加健康。",
-		"吃饱了才有力气干活！",
-		"时间紧迫！",
-		"吃货们，",
-		"报餐不仅是一种行为，更是一种态度，",
-		"还没报餐的同学，你们的午餐/晚餐有着落了吗？",
-		"报餐啦！报餐啦！",
-		"Hi 大家，",
-		"嗨嗨~",
-	}
-	randomIndex := rand.Intn(len(msgs))
+func GeneratePrevOrderingMsg(gemini_api ...string) string {
+	var msg string
 
-	return msgs[randomIndex]
+	gemini_api_key := ""
+	if len(gemini_api_key) > 0 {
+		gemini_api_key = gemini_api[0]
+	}
+	legacy_mode := gemini_api[0] == ""
+
+	if gemini_api_key != "" {
+		msg = gemini_api[0] // TODO: get msg from Gemini
+		// TODO: if get msg from Gemini failed, use legacy mode instead
+	}
+	if legacy_mode {
+		// legacy process when gemini_api
+		msgs := []string{
+			"报餐时间到！",
+			"爱护身体是一种投资，要投资你的胃，让身体更加健康。",
+			"吃饱了才有力气干活！",
+			"时间紧迫！",
+			"吃货们，",
+			"报餐不仅是一种行为，更是一种态度，",
+			"还没报餐的同学，你们的午餐/晚餐有着落了吗？",
+			"报餐啦！报餐啦！",
+			"Hi 大家，",
+			"嗨嗨~",
+		}
+		randomIndex := rand.Intn(len(msgs))
+		msg = msgs[randomIndex]
+	}
+
+	return msg
 }
 
-func GenerateOrderingNotificationMsg() string {
-	msgs := []string{
-		"工作再忙也要记得报餐哦~",
-		"别忘记报餐哦~",
-		"快点报餐啦~",
-		"请尽快完成报餐，谢谢合作！",
-		"快去快去报餐！让我们一起为美食而战！",
-		"快去快去报餐！让我们一起分享美食的快乐！",
-		"快来报餐吧~",
-		"报餐报餐！请大家积极配合",
-		"赶紧报餐，美味等你！",
-		"报餐时间到，别错过哦！",
-		"今天你报餐了吗？",
-		"午餐/晚餐报餐进行中，请及时执行。",
-		"为了你的胃，请尽快报餐！",
-		"报餐啦！错过今天再等明天！",
-		"亲爱的同学们，请尽快完成报餐。",
-		"每天一餐，快乐一天！快来报餐吧。",
-	}
-	randomIndex := rand.Intn(len(msgs))
+func GenerateOrderingNotificationMsg(gemini_api ...string) string {
+	var msg string
 
-	return GeneratePrevOrderingMsg() + msgs[randomIndex] + GenerateKaomoji()
+	gemini_api_key := ""
+	if len(gemini_api_key) > 0 {
+		gemini_api_key = gemini_api[0]
+	}
+	legacy_mode := gemini_api[0] == ""
+
+	if gemini_api_key != "" {
+		msg = gemini_api[0] // TODO: get msg from Gemini
+		// TODO: if get msg from Gemini failed, use legacy mode instead
+	}
+	if legacy_mode {
+		// legacy process
+		msgs := []string{
+			"工作再忙也要记得报餐哦~",
+			"别忘记报餐哦~",
+			"快点报餐啦~",
+			"请尽快完成报餐，谢谢合作！",
+			"快去快去报餐！让我们一起为美食而战！",
+			"快去快去报餐！让我们一起分享美食的快乐！",
+			"快来报餐吧~",
+			"报餐报餐！请大家积极配合",
+			"赶紧报餐，美味等你！",
+			"报餐时间到，别错过哦！",
+			"今天你报餐了吗？",
+			"午餐/晚餐报餐进行中，请及时执行。",
+			"为了你的胃，请尽快报餐！",
+			"报餐啦！错过今天再等明天！",
+			"亲爱的同学们，请尽快完成报餐。",
+			"每天一餐，快乐一天！快来报餐吧。",
+		}
+		randomIndex := rand.Intn(len(msgs))
+		msg = GeneratePrevOrderingMsg() + msgs[randomIndex] + GenerateKaomoji()
+	}
+
+	return msg
 }
 
 func GeneratePrevMealMsg() string {
